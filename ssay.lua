d5a710d4fc9164ea920723702a86d7608b4bae99
zgww <645789309@qq.com>
2016-08-17 20:31:30 +0800

    add more fs api: mkdir rmdir unlink rename stat scandir

--- ssay.lua
+++ ssay.lua
@@ -5,6 +5,12 @@ local snuv = require 'snuv'
 local yield = profile.yield
 local resume = profile.resume
 
+local function _c(fn, h, s, ...)
+	fn(h, s, ...)
+	local succ, msg, sz = yield('CALL', s)
+	return msg
+end
+
 
 
 skynet.start(function ()
@@ -14,6 +20,40 @@ skynet.start(function ()
 
 		local session = skynet.genid()
 		local handle = skynet.self()
+		local h, s  = handle, session
+
+		--[[
+		local msg = _c(snuv.mkdir, h, s, '_mkdir')
+		print(msg, snuv.get_result(msg))
+
+		local msg = _c(snuv.rmdir, h, s, '_mkdir')
+		print(msg, snuv.get_result(msg))
+
+		local msg = _c(snuv.unlink, h, s, 'xx')
+		print(msg, snuv.get_result(msg))
+		local msg = _c(snuv.rename, h, s, 'xx', 'xxxxxx')
+		print(msg, snuv.get_result(msg))
+		local msg = _c(snuv.stat, h, s, 'xxxxxx')
+		print(msg, snuv.get_result(msg),
+			snuv.get_mtime_ms(msg),
+			snuv.get_atime_ms(msg),
+			snuv.get_ctime_ms(msg)
+		)
+		]]
+		local msg = _c(snuv.scandir, h, s, 'xx')
+		while true do
+			local result = snuv.get_result(msg)
+			print('lua get result ', result)
+			if result <= 0 then break end
+
+			local str = snuv.get_str(msg)
+			print('str', str)
+
+			_, msg = yield('CALL', s)
+		end
+		print('scandir end')
+
+		--[[
 		snuv.spawn(handle, session, {
 			'ls', '-al'
 		})
@@ -30,7 +70,6 @@ skynet.start(function ()
 			end
 		end
 		print('spawn done')
-		--[[
 
 		snuv.open(handle, session, 'Makefile', 'w+')
 		local succ, msg, sz = yield('CALL', session)
@@ -62,6 +101,5 @@ skynet.start(function ()
 		print('cmd  ', snuv.get_cmd(msg))
 		print('str  ', snuv.get_str(msg))
 		]]
-
 	end)
 end)
